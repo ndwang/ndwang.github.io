a8632f932b051ab8cdc5d4cf6e39fd96ddc13c80
ndwang <45082410+ndwang@users.noreply.github.com>
2025-07-06 06:10:12 +0000

    Deploying to gh-pages from @ ndwang/ndwang.github.io@29b8f381359a52497bf345534c3c1da29c8f39b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -160,13 +160,6 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/2137533", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
