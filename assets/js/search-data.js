9941f85fb07788d220bf2a500c43816260be2d0d
ndwang <45082410+ndwang@users.noreply.github.com>
2025-07-18 03:54:29 +0000

    Deploying to gh-pages from @ ndwang/ndwang.github.io@d22dc699d94975d25acd4fbf85c399ac6970097d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,18 +49,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-spacecharge-jl",
+            },},{id: "projects-spacecharge-jl",
           title: 'SpaceCharge.jl',
           description: "A high-performance Julia package for space charge field calculation with GPU acceleration.",
           section: "Projects",handler: () => {
